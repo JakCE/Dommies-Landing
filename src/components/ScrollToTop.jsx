14591a8a2ba59253db01940af95e768eff3467cd
jak.campos <practicas.0020@sesitdigital.com>
2025-01-22 17:32:46 -0500

    add arrow up component

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Check scroll position to show/hide button
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  // Scroll back to the top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-5 right-5 w-12 h-12 bg-purple-800 border-2 border-purple-800 text-white flex items-center justify-center rounded-lg shadow-lg hover:bg-purple-700 transition-all"
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
+            </svg>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
