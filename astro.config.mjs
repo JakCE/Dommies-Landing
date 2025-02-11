3949d01da7934aa00c1e8641c160a89d78e58ac6
jak.campos <practicas.0020@sesitdigital.com>
2025-02-11 12:51:46 -0500

    add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
+import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), icon()]
+  site: 'https://dommies.pro',
+  integrations: [tailwind(), react(), icon(), sitemap()]
 });
